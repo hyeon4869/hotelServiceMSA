63bcf4c94282de9b84968b59656a6c86f867eda1
hyeon4869 <119660070+hyeon4869@users.noreply.github.com>
2024-02-09 04:07:46 +0900

    server4

--- gateway/src/main/java/hotel/auth/AuthorizationFilter.java
+++ gateway/src/main/java/hotel/auth/AuthorizationFilter.java
@@ -19,91 +19,79 @@ import java.util.Map;
 @Component
 public class AuthorizationFilter extends AbstractGatewayFilterFactory<AuthorizationFilter.Config> {
 
-    private final String jwtSecret = JwtProperties.SECRET; // 토큰 생성 시 사용한 시크릿 키
+    private final String jwtSecret = JwtProperties.SECRET;
 
     public AuthorizationFilter() {
         super(Config.class);
     }
-
     @Override
     public GatewayFilter apply(Config config) {
         return (exchange, chain) -> {
-            // 헤더에서 토큰 추출
-            String token = exchange.getRequest().getHeaders().getFirst("Authorization");
-            System.out.println(
-                    "======================================================================================" + token);
-            // 토큰을 이용한 인증 및 권한 부여 로직 구현
-            if (isValidToken(token)) {
-                return chain.filter(exchange);
-            } else {
-                if (token != null) {
-                    token = token.substring(7);
-                    String payload = token.split("\\.")[1];
-                    String json = new String(Base64.getDecoder().decode(payload));
-                    String username = new JSONObject(json).getString("username");
-                    String refreshToken = getRefreshToken(username);
-                    String newToken = requestNewToken(refreshToken);
-                    System.out.println("=====================================================리프레시 토큰  "+refreshToken);
-                    System.out.println("=====================================================재발급된 엑세스 토큰  "+newToken);
-                    
-
-                                    // 응답 헤더에 새 토큰 추가
-                exchange.getResponse().getHeaders().add("newauthorization", "Bearer " + newToken);
-
-                }
-
+            String authorizationHeader = exchange.getRequest().getHeaders().getFirst("Authorization");
+    
+            if (authorizationHeader == null || !authorizationHeader.startsWith("Bearer ")) {
                 exchange.getResponse().setStatusCode(HttpStatus.UNAUTHORIZED);
                 return exchange.getResponse().setComplete();
             }
-        };                
+    
+            String token = authorizationHeader.substring(7);
+    
+            if (isTokenExpired(token)) {
+                String payload = token.split("\\.")[1];
+                String json = new String(Base64.getDecoder().decode(payload));
+                String username = new JSONObject(json).getString("username");
+                String refreshToken = getRefreshToken(username);
+                String newToken = requestNewToken(refreshToken);
+    
+                exchange.getResponse().getHeaders().add("newauthorization", "Bearer " + newToken);
+            }
+    
+            return chain.filter(exchange);
+        };
     }
-
-    private boolean isValidToken(String token) {
-        if (token == null || !token.startsWith("Bearer ")) {
-            return false;
-        }
-
+    
+    private boolean isTokenExpired(String token) {
         try {
             token = token.substring(7);
 
-            Map<String, Claim> claims = JWT.require(Algorithm.HMAC512(jwtSecret.getBytes()))
+            JWT.require(Algorithm.HMAC512(jwtSecret.getBytes()))
                     .build()
                     .verify(token)
                     .getClaims();
 
-            return true;
+            return false;
         } catch (TokenExpiredException e) {
-            return false;
+            return true;
         } catch (Exception e) {
-            return false;
+            return true;
         }
     }
+
     private String getRefreshToken(String username) {
-        WebClient webClient = WebClient.create("http://localhost:8088"); // user의 URL
-        String refreshToken = webClient.get() // GET 요청
+        WebClient webClient = WebClient.create("http://localhost:8088");
+        String refreshToken = webClient.get()
                 .uri(uriBuilder -> uriBuilder
                         .path("/users/{username}/refreshToken")
-                        .build(username)) // 사용자 아이디를 이용하여 리프레시 토큰 가져오는 API의 경로
-                .retrieve() // 응답 받아오기
-                .bodyToMono(String.class) // 응답 본문을 String으로 변환
-                .block(); // Mono의 결과를 블로킹 방식으로 받아오기
+                        .build(username))
+                .retrieve()
+                .bodyToMono(String.class)
+                .block();
 
         return refreshToken;
     }
 
     private String requestNewToken(String refreshToken) {
-        WebClient webClient = WebClient.create("http://localhost:8088"); // User 마이크로서비스의 url
-        String newToken = webClient.post() // POST 요청
-                .uri("/users/token/refresh") // 토큰 재발급 요청 경로
-                .body(BodyInserters.fromValue(refreshToken)) // 요청 본문에 리프레시 토큰 삽입
-                .retrieve() // 응답 받아오기
-                .bodyToMono(String.class) // 응답 본문을 String으로 변환
-                .block(); // Mono의 결과를 블로킹 방식으로 받아오기
+        WebClient webClient = WebClient.create("http://localhost:8088");
+        String newToken = webClient.post()
+                .uri("/users/token/refresh")
+                .body(BodyInserters.fromValue(refreshToken))
+                .retrieve()
+                .bodyToMono(String.class)
+                .block();
 
         return newToken;
     }
 
     public static class Config {
-        // 필요한 경우 구성 옵션을 추가할 수 있습니다.
     }
 }
